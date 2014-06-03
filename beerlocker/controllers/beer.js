fae51bf7002f0d745dd43156b95b116d338545fa
vic <git@dfhu.org>
2014-06-03 18:53:09 -0400

    add ownership to beer locker

--- beerlocker/controllers/beer.js
+++ beerlocker/controllers/beer.js
@@ -5,6 +5,7 @@ exports.postBeers = function (req, res) {
   beer.name = req.body.name;
   beer.type = req.body.type;
   beer.quantity = req.body.quantity;
+  beer.userId = req.user._id;
 
   beer.save(function(err) {
     if (err) {
@@ -17,49 +18,50 @@ exports.postBeers = function (req, res) {
 
 
 exports.getBeers = function (req, res) {
-  Beer.find(function(err, beers) {
-    if (err) {
-      res.send(err);
-    }
-    res.json(beers);
-  });
+  Beer.find(
+    {userId: req.user._id},
+    function(err, beers) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(beers);
+    });
 };
 
 exports.getBeer = function (req, res) {
-  Beer.findById(req.params.beer_id, function (err, beer) {
-    if (err) {
-      res.send(err);
-    }
-    res.json(beers);
-  });
+  Beer.find(
+    {userId: req.user._id, _id: req.params.beer_id}, 
+    function (err, beer) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(beers);
+    });
 };
 
 
 exports.putBeer = function (req, res) {
-  Beer.findById(req.params.beer_id, function (err, beer) {
-    if (err) {
-      res.send(err);
-    }
-
-    beer.quantity = req.body.quantity;
-
-    beer.save(function(err) {
+  Beer.update(
+    {userId: req.user._id, _id: req.params.beer_id},
+    {quantity: req.body.quantity},
+    function (err, num, raw) {
       if (err) {
         res.send(err);
       }
-      res.json(beer);
-    });
+      res.json({message: num + ' updated'});
   });
 };
 
 
 exports.deleteBeer = function (req, res) {
-  Beer.findById(req.params.beer_id, function (err, beer) {
-    if (err) {
-      res.send(err);
-    }
-    
-    res.json({message: 'Removed beer'});
-  });
+  Beer.remove(
+    {userId: req.user._id, _id: req.params.beer_id},
+    function (err, beer) {
+      if (err) {
+        res.send(err);
+      }
+      
+      res.json({message: 'Removed beer'});
+    });
 };
 
