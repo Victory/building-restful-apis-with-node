5e9118d9bf549c09f51a57f69e2eab85f434a016
vic <git@dfhu.org>
2014-06-06 17:50:29 -0400

    PUT in curl-beer-run

--- beerlocker/curl-beer-run.sh
+++ beerlocker/curl-beer-run.sh
@@ -28,3 +28,9 @@ echo $PAGE
 PAGE=$(curl -u "bad:guy" http://localhost:9000/api/beers)
 
 echo $PAGE
+
+beers=$(curl -u "$AUTH" http://localhost:9000/api/beers)
+beerId=$(echo "<?php \$beers = json_decode('$beers'); echo \$beers[0]->_id; ?>" | php);
+
+DATAURL="quantity=11"
+curl -u "$AUTH" -d "$DATAURL" -X PUT http://localhost:9000/api/beers/$beerId
